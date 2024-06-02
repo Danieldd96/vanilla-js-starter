cebf0ad22474f228e214f635d754297df7d9788a
Danieldd96 <danieldelgado.dd270u@gmail.com>
2024-06-01 18:17:09 -0600

    Creacion de boton de eliminar y estilos de pagina

--- src/index.js
+++ src/index.js
@@ -6,10 +6,30 @@ const lista = document.getElementById('container')  ///Elemento :Ul
 boton.addEventListener("click",(e) =>{  
     e.preventDefault();
     const texto = contenido.value;    ///El const me ayudara para obtener el valor del input llamado contenido.
-    const li = document.createElement("li"); ///Este const me ayuda a crear un elemento "li".
-    const p = document.createElement("p"); ///Este const me ayuda a crear un elemento "p".
-    p.textContent = texto;   ///Aqui estoy Haciendo que el const p que se vaya a crear tenga el contenido del input.
-    li.appendChild(p);      ///En este busca en li y agrega el const "p" el cual crea un elemento en el html.
-    lista.appendChild(li); ///Aqui busca en un "ul" con un id "lista" y agrega el const "li" el cual crea un elemento en el html.
+    if (texto !== "") {
+        const li = document.createElement("h2"); ///Este const me ayuda a crear un elemento "li".
+        const checkbox = document.createElement("input")
+        const p = document.createElement("label"); ///Este const me ayuda a crear un elemento "p".
+        checkbox.type= "checkbox"
+        checkbox.className="check"
+        
+        p.textContent = texto;   ///Aqui estoy Haciendo que el const p que se vaya a crear tenga el contenido del input.
+        li.appendChild(checkbox)
+        li.appendChild(p);      ///En este busca en li y agrega el const "p" el cual crea un elemento en el html.
+        li.appendChild(ButonDeBorrar())
+        lista.appendChild(li); ///Aqui busca en un "ul" con un id "lista" y agrega el const "li" el cual crea un elemento en el html.
+        contenido.value = ""
+        
+    }
 })
+function ButonDeBorrar() {
+    const BtnBorrar = document.createElement("button")
+    BtnBorrar.textContent= "X"
+    BtnBorrar.className = "BotonBorrar"
+    BtnBorrar.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        lista.removeChild(item);
+    })
+    return BtnBorrar
+}
 console.log("hola")
